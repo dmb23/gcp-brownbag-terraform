88995d2172fdd630447bd8167f620cabb28b462b
Mischa Ungermann <mischa@dataworkz.de>
2025-04-17 16:55:20 +0200

    import existing project

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "google" {
-  project = var.project
+  project = var.project_id
   region  = var.region
   zone    = var.zone
 }
@@ -18,4 +18,21 @@ module "gcp-cicd" {
   source = "./modules/gcp-cicd"
 
   location = var.region
+}
+
+import {
+  to = google_project.default
+  id = var.project_id
+}
+
+resource "google_project" "default" {
+  auto_create_network = true
+  billing_account     = var.billing_account
+  deletion_policy     = "PREVENT"
+  folder_id           = null
+  labels              = {}
+  name                = var.project_name
+  org_id              = null
+  project_id          = var.project_id
+  tags                = null
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
-variable "project" {}
+variable "project_id" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "billing_account" {
+  type = string
+}
 
 variable "region" {
   default = "europe-west9"
