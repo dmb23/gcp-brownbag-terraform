fa3433fa12ca65a7c0a4452cb9d993c66fce7e38
Mischa Ungermann <mischa@dataworkz.de>
2025-04-22 15:46:09 +0200

    fix: include necessary API

--- main.tf
+++ main.tf
@@ -20,5 +20,11 @@ module "gcp-cicd" {
   location = var.region
 }
 
+resource "google_project_service" "resource_manager_service" {
+  service                    = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
+}
+
 data "google_project" "project" {
+  depends_on = [google_project_service.resource_manager_service]
 }
