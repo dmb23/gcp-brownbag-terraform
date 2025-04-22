f821a129ad996173d49b5103307c5643028ba8e1
Mischa Ungermann <mischa@dataworkz.de>
2025-04-22 14:39:16 +0200

    include tf plan trigger

---
+++ modules/gcp-cicd/triggers.tf
@@ -0,0 +1,28 @@
+resource "google_cloudbuild_trigger" "plan-trigger" {
+  description        = "terraform plan"
+  disabled           = false
+  filename           = "modules/gcp-cicd/cloudbuild-plan.yaml"
+  filter             = null
+  ignored_files      = []
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  included_files     = []
+  location           = "global"
+  name               = "plan-trigger"
+  project            = "focus-dragon-457009-i8"
+  service_account    = google_service_account.cloudbuild_service_account.id
+  substitutions      = {}
+  tags               = []
+  approval_config {
+    approval_required = false
+  }
+  github {
+    enterprise_config_resource_name = null
+    name                            = "gcp-brownbag-terraform"
+    owner                           = "dmb23"
+    push {
+      branch       = "^main$"
+      invert_regex = true
+      tag          = null
+    }
+  }
+}
