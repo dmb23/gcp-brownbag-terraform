b7d57647ef8a6a4021911ed38c2e353cda90dfaa
Mischa Ungermann <mischa@dataworkz.de>
2025-04-22 12:59:17 +0200

    add service account (with too many rights ftm)

--- modules/gcp-cicd/variables.tf
+++ modules/gcp-cicd/variables.tf
@@ -1,4 +1,12 @@
 variable "location" {
   description = "GCS Location for the storage bucket"
-  type = string
+  type        = string
+}
+variable "project_id" {
+  description = "Project ID for GCP project"
+  type        = string
+}
+variable "project_number" {
+  description = "Project Number for GCP project"
+  type        = string
 }
