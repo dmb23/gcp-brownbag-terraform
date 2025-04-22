eae9bb1ab190cba047eea8aa72cf59bf97ba2f2f
Mischa Ungermann <mischa@dataworkz.de>
2025-04-22 16:21:49 +0200

    new trigger for deployments at main

--- modules/gcp-cicd/triggers.tf
+++ modules/gcp-cicd/triggers.tf
@@ -1,5 +1,9 @@
+locals {
+  terraform_version = "1.11"
+}
+
 resource "google_cloudbuild_trigger" "plan-trigger" {
-  description        = "terraform plan"
+  description        = "terraform plan for all non-main branches"
   disabled           = false
   filename           = "modules/gcp-cicd/cloudbuild-plan.yaml"
   filter             = null
@@ -8,10 +12,12 @@ resource "google_cloudbuild_trigger" "plan-trigger" {
   included_files     = []
   location           = "global"
   name               = "plan-trigger"
-  project            = "focus-dragon-457009-i8"
+  project            = data.google_project.project.project_id
   service_account    = google_service_account.cloudbuild_service_account.id
-  substitutions      = {}
-  tags               = []
+  substitutions = {
+    _TF_VERSION = local.terraform_version
+  }
+  tags = []
   approval_config {
     approval_required = false
   }
@@ -21,7 +27,35 @@ resource "google_cloudbuild_trigger" "plan-trigger" {
     push {
       branch       = "^main$"
       invert_regex = true
-      tag          = null
+    }
+  }
+}
+
+
+resource "google_cloudbuild_trigger" "apply-trigger" {
+  description        = "terraform apply for main branch"
+  disabled           = false
+  filename           = "modules/gcp-cicd/cloudbuild-apply.yaml"
+  filter             = null
+  ignored_files      = []
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  included_files     = []
+  location           = "global"
+  name               = "apply-trigger"
+  project            = data.google_project.project.project_id
+  service_account    = google_service_account.cloudbuild_service_account.id
+  substitutions = {
+    _TF_VERSION = local.terraform_version
+  }
+  tags = []
+  approval_config {
+    approval_required = false
+  }
+  github {
+    name  = "gcp-brownbag-terraform"
+    owner = "dmb23"
+    push {
+      branch = "^main$"
     }
   }
 }
