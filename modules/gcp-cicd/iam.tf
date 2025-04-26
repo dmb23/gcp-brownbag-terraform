ed155cea6996c063ca977528b7c6302bfa428f81
Mischa Ungermann <mischa@dataworkz.de>
2025-04-26 13:45:39 +0200

    reduce IAM roles for TF account

--- modules/gcp-cicd/iam.tf
+++ modules/gcp-cicd/iam.tf
@@ -1,10 +1,23 @@
+locals {
+  iam_roles = [
+    "roles/cloudbuild.builds.builder",
+    "roles/iam.serviceAccountUser",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.securityAdmin",
+    "roles/storage.admin",
+    "roles/serviceusage.serviceUsageAdmin",
+  ]
+}
+
 resource "google_service_account" "cloudbuild_service_account" {
   account_id   = "cloudbuild-sa"
   display_name = "Cloudbuild Service Account"
 }
 
-resource "google_project_iam_member" "project_editor" {
+resource "google_project_iam_member" "project_iam_role" {
+  for_each = toset(local.iam_roles)
+
   project = data.google_project.project.project_id
-  role    = "roles/editor"
+  role    = each.value
   member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
