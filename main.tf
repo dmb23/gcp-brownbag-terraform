5809ac850ba7de35d190e31cb5674ab1149dd8bb
Mischa Ungermann <mischa@dataworkz.de>
2025-04-26 14:38:39 +0200

    actually use the new module

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ module "gcp-cicd" {
   location = var.region
 }
 
+module "gcp-agent" {
+  source = "./modules/agent/"
+}
+
 data "google_project" "project" {
   depends_on = [google_project_service.project_service["cloudresourcemanager.googleapis.com"]]
 }
